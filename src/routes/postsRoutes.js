0cd7a2720e0ce1df554d93ec1676779dcf7c40b4
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-11-24 21:08:29 -0300

    feat(routes): add Multer configuration for file uploads in postsRoutes

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,17 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); 
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); 
+    }
+  });
+
+const upload = multer({ storage: storage });
 
 const routes = (app) => {
     app.use(express.json());
@@ -7,6 +19,8 @@ const routes = (app) => {
     app.get("/posts", listarPosts);
 
     app.post("/posts", postarNovoPost);
+
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
